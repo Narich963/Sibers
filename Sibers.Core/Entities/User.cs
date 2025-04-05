4c9854298ee4951e6419a07b85dd444e516d9c38
Nariman Biiazbekov <nariman13.02.2005@gmail.com>
2025-04-05 18:57:45 +0600

    Added Projects field to User

--- Sibers.Core/Entities/User.cs
+++ Sibers.Core/Entities/User.cs
@@ -7,4 +7,10 @@ public class User : IdentityUser<int>
     public string FirstName { get; set; }
     public string MiddleName { get; set; }
     public string LastName { get; set; }
+
+    public List<Project> Projects { get; set; }
+    public User()
+    {
+        Projects = new();
+    }
 }
